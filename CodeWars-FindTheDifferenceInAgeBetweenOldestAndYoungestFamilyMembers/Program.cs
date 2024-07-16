4e8e4d9ee859131ae41de3dca934cc650766f10e
Incyaa <incyaa@outlook.com>
2024-07-16 13:33:10 +0300

    Initial commit

---
+++ CodeWars-FindTheDifferenceInAgeBetweenOldestAndYoungestFamilyMembers/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace CodeWars_FindTheDifferenceInAgeBetweenOldestAndYoungestFamilyMembers;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
